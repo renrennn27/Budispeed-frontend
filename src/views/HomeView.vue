544ad8c82982f6c2463bec6b740835483fe60384
renrennn27 <renoalifprasetyo20@gmail.com>
2025-10-10 23:51:42 +0700

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import Landingpage from '../components/Landingpage.vue'
+import Navbar from '../components/Navbar.vue'
+import Pageproducts from '../components/Pageproducts.vue'
+import Footer from '../components/Footer.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <Landingpage />
+    <Navbar />
+    <Pageproducts />
+    <Footer />
+  </main>
+</template>
